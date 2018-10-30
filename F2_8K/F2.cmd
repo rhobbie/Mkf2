bd862442fe19501d84e83ed7aa3d9be77c60f496
rhobbie <38581300+rhobbie@users.noreply.github.com>
2018-10-30 14:49:26 +0100

    removed error message

--- F2_8K/F2.cmd
+++ F2_8K/F2.cmd
@@ -1,5 +1,5 @@
 @echo off
-del /q SourceDeck.cbn Punched.cbn LP.txt "%~dpn1.cbn"
+del /q SourceDeck.cbn Punched.cbn LP.txt "%~dpn1.cbn" >nul
 Punch "%1" SourceDeck.cbn >nul
 Sim704 F2.xml
 if exist Punched.cbn CleanDeck Punched.cbn "%~dpn1.cbn" >nul
